87d331c40ac3fee362a89d881e80947dcc206c7c
Felix Odete <felix.odette@gmail.com>
2022-08-03 21:01:40 +0300

    Add rentals to person

--- person.rb
+++ person.rb
@@ -7,6 +7,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   attr_accessor :name, :age
@@ -18,6 +19,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals << rental
   end
 
   private
