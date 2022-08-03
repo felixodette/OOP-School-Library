2945a7884ac38e22a3bbe7717d5f06c4d7f8470c
Felix Odete <felix.odette@gmail.com>
2022-08-03 08:35:34 +0300

    Add student class

---
+++ student.rb
@@ -0,0 +1,12 @@
+require "./person.rb"
+
+class Student < Person
+  def initialize(name = "Unknown", classroom, age, parent_permission = true)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\\(ツ)/¯"
+  end
+end
