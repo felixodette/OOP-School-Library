6667102e5940d4a834026ed2f336ca98d95fe97d
Felix Odete <felix.odette@gmail.com>
2022-08-03 08:30:55 +0300

    Add person class

---
+++ person.rb
@@ -0,0 +1,21 @@
+class Person
+  def initialize(name = "Unknown", age, parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def is_of_age?
+    @age >= 18 ? true : false
+  end
+
+  private :is_of_age?
+
+  def can_use_services?
+    @age >= 18 || @parent_permission ? true : false
+  end
+end
