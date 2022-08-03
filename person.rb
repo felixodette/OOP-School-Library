6536a39e41e166cc4abe3ac77ef8848493520ee9
Felix Odete <felix.odette@gmail.com>
2022-08-03 18:30:57 +0300

    Add trimmer decorator

--- person.rb
+++ person.rb
@@ -25,3 +25,10 @@ class Person < Nameable
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+capitalizedTrimmedPerson.correct_name
