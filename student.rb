3008b9db171badfbec4c10af0bad3538687a80e1
Felix Odete <felix.odette@gmail.com>
2022-08-03 20:59:04 +0300

    Add classroom to student

--- student.rb
+++ student.rb
@@ -1,12 +1,19 @@
 require './person'
+require './classroom'
 
 class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(name, age, parent_permission)
     @classroom = classroom
+    @classroom = Classroom.new(classroom)
   end
 
   def play_hooky
     '¯\\(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 end
